9af05af5bd620fd13450a9a75b76e80ce73c254d
Dell <Dell@DESKTOP-6D8N9BG>
2026-03-17 02:28:16 +0700

    add redis cache-aside logic to redirect endpoint

--- UrlManagement.Api/Program.cs
+++ UrlManagement.Api/Program.cs
@@ -1,32 +1,25 @@
 using MongoDB.Driver;
+using StackExchange.Redis; 
 using UrlManagement.Api.Settings;
 using UrlManagement.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-// Configure MongoDB
 var mongoDbSettings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
 builder.Services.AddSingleton(mongoDbSettings!);
 builder.Services.AddSingleton<IMongoClient>(new MongoClient(mongoDbSettings!.ConnectionString));
 
-builder.Services.AddScoped<IUrlService, UrlService>();
 
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect("localhost:6379"));
+
+builder.Services.AddScoped<IUrlService, UrlService>();
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
+if (app.Environment.IsDevelopment()) { app.MapOpenApi(); }
 app.UseHttpsRedirection();
-
 app.MapControllers();
-
 app.Run();
