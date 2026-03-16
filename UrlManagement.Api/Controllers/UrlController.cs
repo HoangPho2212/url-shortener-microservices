4535b35c2542fc5ef1905d25283916d6ea295b45
Dell <Dell@DESKTOP-6D8N9BG>
2026-03-17 00:58:13 +0700

    implement Base62 short code generation algorithm-sprint2

---
+++ UrlManagement.Api/Controllers/UrlController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrlManagement.Api.Utils;
+
+namespace UrlManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/url")]
+public class UrlController : ControllerBase
+{
+    [HttpPost("shorten")]
+    public IActionResult ShortenUrl([FromBody] long id)
+    {
+        
+        var code = ShortCodeGenerator.Encode(id);
+        return Ok(new { ShortCode = code });
+    }
+}
---
+++ UrlManagement.Api/Utils/ShortCodeGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace UrlManagement.Api.Utils;
+
+public static class ShortCodeGenerator
+{
+    private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    public static string Encode(long input)
+    {
+        if (input == 0) return Alphabet[0].ToString();
+
+        var shortCode = string.Empty;
+        while (input > 0)
+        {
+            shortCode = Alphabet[(int)(input % 62)] + shortCode;
+            input /= 62;
+        }
+        return shortCode;
+    }
+}
